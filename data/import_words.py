3c2610fb0c4e7dd19b6f9173389429bdf754b7b9
GodwinEke <ekegodwinc@gmail.com>
2022-01-04 00:16:47 -0500

    4/01/2022 finished 'check', 'eligible', 'define' ... KEYWORDS

--- data/import_words.py
+++ data/import_words.py
@@ -2,21 +2,22 @@ import csv
 import sqlite3
 
 #create database
-open("eng_words.db", "w").close()
-con = sqlite3.connect("eng_words.db")
+open("japanese_words.db", "w").close()
+con = sqlite3.connect("japanese_words.db")
 cursor= con.cursor()
 
 #create table
-cursor.execute("CREATE TABLE words_en (id INTEGER, words TEXT not NULL, meaning TEXT, PRIMARY KEY(id))")
+cursor.execute("CREATE TABLE words_japan (id INTEGER, words TEXT , english_translation TEXT, PRIMARY KEY(id))")
 
 #open english_txt file and insert values
 counter = 1
-with open("english_words.txt", "r") as file:
+with open("D:/Documents/finalproject/data/wordstxt/japanese_words.txt", "r") as file:
     reader = csv.reader(file)
 
     for row in reader:
-        cursor.execute("INSERT INTO words_en (id, words) VALUES(?, ?)", (counter, row[0]))
+        cursor.execute("INSERT INTO words_japan(words) VALUES(:translation)", {"translation":row[0]})
         counter+=1
+
 
 #save file
 con.commit()
